aeff758f56ca7ed7aae8b917e18bb31ca8fe20b6
MichaelGuanoluisa <66236038+MichaelGuanoluisa@users.noreply.github.com>
2020-07-09 16:07:07 -0500

    Update SumOddsRange.java

--- SumOddsRange.java
+++ SumOddsRange.java
@@ -2,24 +2,17 @@ import java.util.Scanner;
 
 /**
 Pedir 2 numeros al usuario:
-
 int num1;
 int num2;
-
-
 Puedes asumir que num2 > num1
-
 Escribe un programa que calcule e imprima la suma de los numeros impares que estan entre num1 y num2 incluyendo estos numeros.
-
 Ejemplo de entrada/salida:
 #1: 3
 #2: 11
 35
-
 #1: 4
 #2: 20
 96
-
 #1: -2
 #2: 6
 8
@@ -32,7 +25,12 @@ class SumOddsRange {
     System.out.print("#2: ");
     int num2 = s.nextInt();
     //start below
-    
-    
-  }
+    int sum = 0;
+    for(int i=num1; i<=num2; i++){
+        if(i % 2 != 0){
+            sum += i;
+        }
+    }
+      System.out.println(sum);
+    }
 }
